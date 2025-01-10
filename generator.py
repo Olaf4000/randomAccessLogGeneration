552931e6db9cf73d0924a50c63bccfec0dec11bb
danielbenfer <daniel-l.benfer@gmx.de>
2025-01-11 00:03:51 +0100

    removed .env because shit more stuff i can´t be bothered to explain

--- generator.py
+++ generator.py
@@ -1,14 +1,11 @@
-from dotenv import load_dotenv
-import os
+import json
 
 from access_log_generation import generate_access_logs
 
-load_dotenv()
-
-amount_of_ips = os.getenv("AMOUNT_OF_IPS")
-amount_of_hosts = os.getenv("AMOUNT_OF_HOSTS")
-amount_of_anomalies = os.getenv("AMOUNT_OF_ANOMALIES")
-max_occurrence = os.getenv("MAX_OCCURRENCE")
+amount_of_ips = 100
+amount_of_hosts = 10
+amount_of_anomalies = 2
+max_occurrence = 50
 
 logs = generate_access_logs(
     amount_of_ips,
@@ -20,3 +17,11 @@ logs = generate_access_logs(
 with open('logs', 'w') as file:
     for log in logs:
         file.write(log + '\n')
+
+with open("logs.json", "w") as file:
+    file.write('[')
+
+    for log in logs:
+        file.write(log + ',')
+
+    file.write(']')
