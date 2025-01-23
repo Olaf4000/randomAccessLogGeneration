0eae4c7c6eee2e3b25641dde548759fbab84ab93
Olaf4000 <daniel.luca.benfer@gmail.com>
2025-01-23 14:01:22 +0100

    added error handling

--- accessLogAnonymization/anonymise.py
+++ accessLogAnonymization/anonymise.py
@@ -1,22 +1,30 @@
+from shutil import Error
+
 import pandas as pd
 import hashlib
+
+from numpy.f2py.auxfuncs import throw_error
+
 
 # functions
 def sha256_hash(data: str) -> str:
     return hashlib.sha256(data.encode()).hexdigest()
 
 # import and normalize data
-logs_raw = pd.read_json('logs_to_anonymise.json')
+try:
+    logs_raw = pd.read_json('logs_to_anonymise.json')
+except Error as e:
+    raise e
+
 logs_cut = logs_raw[['timestamp', 'action', 'httpRequest', 'host']]
 
 logs_normalized = pd.json_normalize(logs_cut['httpRequest'])
 logs = pd.concat([logs_cut, logs_normalized], axis=1)
 logs.drop(['httpRequest'], axis=1, inplace=True)
 
+# anonymise clientIp
 for row in logs.itertuples():
     logs.at[row.Index, 'clientIp'] = sha256_hash(row[4])
-
-print(logs)
 
 with open("logs_anonymised.json", "w") as file:
     file.write(logs.to_json(orient='records'))
